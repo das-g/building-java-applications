55fb74f760c02ea85ed2694860984accf52585b7
Eric Wendelin <eric@gradle.com>
2018-01-12 12:05:08 -0700

    Convert to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.gradle.build-scan") version "1.11"
+    id("org.gradle.guides.getting-started") version "0.10.3"
+    id("org.gradle.guides.test-jvm-code") version "0.10.3"
+}
+
+configure<org.gradle.guides.GuidesExtension> {
+    setRepoPath("gradle-guides/creating-java-applications")
+    setMainAuthor("Ken Kousen")
+}
+
+buildScan {
+    setLicenseAgreementUrl("https://gradle.com/terms-of-service")
+    setLicenseAgree("yes")
+    if (!System.getenv("CI").isNullOrEmpty()) {
+        publishAlways()
+        tag("CI")
+    }
+}
